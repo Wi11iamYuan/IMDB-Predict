e3a6bab50872a200b4bf935e0c38141f4b160f44
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2023-11-21 22:09:56 -0800

    final product

--- imdb.py
+++ imdb.py
@@ -1,6 +1,7 @@
 # %%
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # %%
 from sklearn.preprocessing import OneHotEncoder
@@ -31,11 +32,11 @@ df['genre'] = df['genre'].str.split(', ')
 
 # %%
 # make sure all movies have a rating
-df['certificate'].fillna('Unrated', inplace=True)
+df['certificate'].fillna('Not Rated', inplace=True)
 
 # %%
 # drop title
-df.drop(['title'], axis=1, inplace=True)
+df.drop(['title','votes'], axis=1, inplace=True)
 
 # %%
 # replace year with last 4 digits
@@ -43,14 +44,16 @@ df['year'] = df['year'].str[-4:].astype(int)
 
 # %%
 # interpolate gross with linear regression on metascore
-m = df['gross'].corr(df['metascore']) * df['gross'].std() / df['metascore'].std()
-b = df['gross'].mean() - m * df['metascore'].mean()
-df['gross'] = df['gross'].fillna(df['metascore'] * m + b)
+# m = df['gross'].corr(df['metascore']) * df['gross'].std() / df['metascore'].std()
+# b = df['gross'].mean() - m * df['metascore'].mean()
+# df['gross'] = df['gross'].fillna(df['metascore'] * m + b)
 
 # %%
 # interpolate metascore with median
 df['metascore'] = df['metascore'].fillna(df['metascore'].median())
+df['gross'] = df['gross'].fillna(df['gross'].median())
 
+#come back to this
 
 # %%
 # one hot encode certificate
@@ -82,41 +85,78 @@ del genre_df
 
 # %%
 # dataframe with all directors and the number of movies they have directed
-director_counts = {}
-for row in df['director']:
-	for director in row:
-		director_counts[director] = director_counts.get(director, 0) + 1
+# director_counts = {}
+# for row in df['director']:
+# 	for director in row:
+# 		director_counts[director] = director_counts.get(director, 0) + 1
 
-director_scores = pd.DataFrame(list(director_counts.items()), columns=['Director', 'Score']).sort_values(by='Score', ascending=False)
-director_scores
+# director_scores = pd.DataFrame(list(director_counts.items()), columns=['Director', 'Score']).sort_values(by='Score', ascending=False)
+# director_scores
 
 # %%
 # new column 'Director Score' that is the sum of the scores of each director
-@lru_cache(maxsize=None)
-def get_director_score(director):
-	return director_scores.loc[director_scores['Director'] == director, 'Score'].values[0]
+# @lru_cache(maxsize=None)
+# def get_director_score(director):
+# 	return director_scores.loc[director_scores['Director'] == director, 'Score'].values[0]
 
-df['Director Score'] = df['director'].apply(lambda x: sum([get_director_score(director) for director in x]))
-df.drop('director', axis=1, inplace=True)
+# df['Director Score'] = df['director'].apply(lambda x: sum([get_director_score(director) for director in x]))
+# df.drop('director', axis=1, inplace=True)
+
+#maybe try avg instead of sum
 
 # %%
 # dataframe with all stars and the number of movies they have starred in
-star_counts = {}
-for row in df['stars']:
-	for star in row:
-		star_counts[star] = star_counts.get(star, 0) + 1
+# star_counts = {}
+# for row in df['stars']:
+# 	for star in row:
+# 		star_counts[star] = star_counts.get(star, 0) + 1
 
-star_scores = pd.DataFrame(list(star_counts.items()), columns=['Star', 'Score']).sort_values(by='Score', ascending=False)
-star_scores
+# star_scores = pd.DataFrame(list(star_counts.items()), columns=['Star', 'Score']).sort_values(by='Score', ascending=False)
+# star_scores
 
 # %%
-# new column 'Star Score' that is the sum of the scores of each star
+# dataframe with all stars and their rating based on interpolated rating
+star_ratings = {}
+for row in df[['stars', 'rating']].itertuples():
+	for star in row.stars:
+		star_ratings[star] = star_ratings.get(star, []) + [row.rating]
+
+star_ratings = {star: np.average(ratings) for star, ratings in star_ratings.items()}
+star_ratings = pd.DataFrame(list(star_ratings.items()), columns=['Star', 'Rating']).sort_values(by='Rating', ascending=False)
+star_ratings[star_ratings['Star'] == "Leonardo DiCaprio"]
+
+# %%
+# dataframe with all directors and their rating based on interpolated rating
+director_ratings = {}
+for row in df[['director', 'rating']].itertuples():
+	for director in row.director:
+		director_ratings[director] = director_ratings.get(director, []) + [row.rating]
+
+director_ratings = {director: np.average(ratings) for director, ratings in director_ratings.items()}
+director_ratings = pd.DataFrame(list(director_ratings.items()), columns=['Director', 'Rating']).sort_values(by='Rating', ascending=False)
+director_ratings[director_ratings['Director'] == "Craig Moss"]
+
+# %%
+# new column 'Star Score' that is the avg of the scores of each star
+
 @lru_cache(maxsize=None)
 def get_star_score(star):
-	return star_scores.loc[star_scores['Star'] == star, 'Score'].values[0]
+	return star_ratings.loc[star_ratings['Star'] == star, 'Rating'].values[0]
 
-df['Star Score'] = df['stars'].apply(lambda x: sum([get_star_score(star) for star in x]))
+df['Star Score'] = df['stars'].apply(lambda x: np.average([get_star_score(star) for star in x]))
 df.drop('stars', axis=1, inplace=True)
+df['Star Score'] = df['Star Score'].fillna(df['Star Score'].median())
+df
+
+# %%
+# new column 'Director Score' that is the avg of the scores of each director
+@lru_cache(maxsize=None)
+def get_director_score(director):
+	return director_ratings.loc[director_ratings['Director'] == director, 'Rating'].values[0]
+
+df['Director Score'] = df['director'].apply(lambda x: np.average([get_director_score(director) for director in x]))
+df.drop('director', axis=1, inplace=True)
+df
 
 # %%
 # standardize all columns (u=0, std=1)
@@ -143,6 +183,33 @@ r2_score(y_test, y_pred)
 # %%
 # plot 10 most important features
 feature_importances = pd.DataFrame(model.feature_importances_, index=x_train.columns, columns=['importance']).sort_values(by='importance', ascending=False)
-feature_importances[:10].plot.bar()
-plt.show()
+# feature_importances[:10].plot.bar()
+# plt.show()
+feature_importances[::-1].plot.barh()
 
+# %%
+# for length in range(1, 21):
+# print(f"Top {length} features:")
+# print(feature_importances[:length].index.values)
+
+# certificate_Not Rated brought score down
+# genre_Family brought score down
+list = []
+for length in range(1, 21):
+	print(f"Top {length} features:")
+	print(feature_importances[:10].index.values)
+	x = df[feature_importances[:10].index.values]
+	y = df['rating']
+
+	x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+	model = XGBRegressor()
+	model.fit(x_train, y_train)
+
+	y_pred = model.predict(x_test)
+	r2_score(y_test, y_pred)
+	list.append(r2_score(y_test, y_pred))
+# %%
+# list
+pd.DataFrame(list).plot.bar()
+# %%
